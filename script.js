bff80e7365e0cd56a595090d1d3479e52b391834
Davyhh <daavyhh@gmail.com>
2023-01-30 19:45:03 +0100

    Funzione per il calcolo del risultato + implementazione nel sito

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+
 function acquisisciValore(obj){
     let input = document.querySelector("input");
     if(input.classList.length==0){
@@ -9,8 +10,14 @@ function acquisisciValore(obj){
     }
     else{
         obj.numero= input.value;
+        let ris = calcolo(obj.base,obj.numero);
         let risultato= document.getElementById("risultato");
+        risultato.innerHTML = "Risultato: ";
+        for(let i=0; i<ris.length;i++)
+            risultato.innerHTML += ris[i];
         risultato.style.display = "block";
+        
+        
 
         
     }   
@@ -19,4 +26,32 @@ function acquisisciValore(obj){
 let obj={
     base:0,
     numero:0
+}
+
+
+function calcolo(base,numero){
+    let ris= [];
+    let i=0;
+    if(base<11){
+        while(numero>0){
+            ris[i] = numero%base;
+            numero = parseInt(numero/base);
+            i++;
+        }
+    }
+    else{
+        let lettere = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", 
+        "L", "M" ,"N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+        while(numero>0){
+            if(numero%base>9)
+                ris[i] = lettere[numero%base-10];
+            else
+                ris[i] = String(numero%base);
+            numero = parseInt(numero/base);
+            i++;
+        }
+    }
+    ris = ris.reverse();
+    return ris;
+    
 }
